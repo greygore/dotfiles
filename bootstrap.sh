ae5bc65687022f4ce8ba991bc85e90cb01c055bf
Gregory Wood <greg@wood.name>
2014-10-08 02:49:17 -0400

    Added missing semicolons and quotes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-git pull
+cd "$(dirname "$0")";
+git pull;
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~;
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
-unset doIt
-source ~/.bash_profile
+		doIt;
+	fi;
+fi;
+unset doIt;
+source ~/.bash_profile;
