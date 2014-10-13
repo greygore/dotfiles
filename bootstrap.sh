f9deb31454167a390713b7781e42713a55ec954e
Gregory Wood <greg@wood.name>
2014-10-13 13:50:59 -0400

    Use configured bash instead of default

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-cd "$(dirname "$0")";
-git pull;
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~;
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -avh --no-perms . ~;
+	source ~/.bash_profile;
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
@@ -13,5 +18,5 @@ else
 		doIt;
 	fi;
 fi;
+
 unset doIt;
-source ~/.bash_profile;
