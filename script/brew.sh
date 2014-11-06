de44b824905f7cc710e17ce9e7a9f2c95b55463e
Gregory Wood <greg@wood.name>
2014-11-06 17:46:29 -0500

    Add lib functions to brew script

--- script/brew.sh
+++ script/brew.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+SOURCE="${BASH_SOURCE[0]}"
+# Resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # If $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DOTFILES_ROOT="$( cd -P "$( dirname "$SOURCE" )"/.. && pwd )"
+cd $DOTFILES_ROOT
+
+source "$DOTFILES_ROOT/script/lib.sh"
+
 # Install Homebrew
 if test ! $(which brew); then
-	echo "Installing homebrew..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	info "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+	&& success 'Homebrew installed.'
 fi
 
 # Upgrade recipes
