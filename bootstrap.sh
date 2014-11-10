9229a7b22e147dd6ee3e766a3d81318173d5ebb6
Gregory Wood <greg@wood.name>
2014-11-10 17:39:56 -0500

    Add background color change to beginning of bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,11 @@ DOTFILES_DEFAULT_DIRECTORY="$HOME/.dotfiles"
 DOTFILES_DIRECTORY=${DOTFILES_DIRECTORY-$DOTFILES_DEFAULT_DIRECTORY}
 DOTFILES_TARBALL_PATH="https://github.com/$DOTFILES_USER/dotfiles/tarball/master"
 
+# Set the terminal background color on OSX
+if [ "$(uname -s)" == "Darwin" ]; then
+	osascript -e 'tell application "Terminal"' -e 'tell selected tab of front window' -e 'set normal text color to {65535,65535,65535}' -e 'set background color to {0,0,0}' -e 'end tell' -e 'end tell'
+fi
+
 # If missing, download and extract the dotfiles repository
 if [[ ! -d $DOTFILES_DIRECTORY ]]; then
 	printf "$(tput bold; tput setaf 7)       Downloading dotfiles...$(tput sgr0)\n"
