6aae1446e377d08ac525d1a6ac2222ed1e67db90
Gregory Wood <greg@wood.name>
2014-11-11 17:41:06 -0500

    Add logging of npm installs to npm.log

--- script/npm.sh
+++ script/npm.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-npm install -g tldr
+SOURCE="${BASH_SOURCE[0]}"
+# Resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # If $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DOTFILES_ROOT="$( cd -P "$( dirname "$SOURCE" )"/.. && pwd )"
+cd $DOTFILES_ROOT
 
-npm install -g ember-cli
+source "$DOTFILES_ROOT/script/lib.sh"
+
+npm install -g tldr  >> "$DOTFILES_ROOT/npm.log" 2>&1
+
+npm install -g ember-cli >> "$DOTFILES_ROOT/npm.log" 2>&1
