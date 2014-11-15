e1ff9940b3a02a57f3be47fdef2dff5063d901ad
Gregory Wood <greg@wood.name>
2014-11-15 10:25:14 -0500

    Silence terminal killing when using another terminal like iTerm2

--- script/install.sh
+++ script/install.sh
@@ -173,4 +173,4 @@ fi
 
 success 'Done! Some changes may require restarting your computer to take effect.'
 pause 'Press any key to close the terminal window so that the profile can be initialized on restart.'
-killall Terminal
+killall Terminal > /dev/null 2>&1
