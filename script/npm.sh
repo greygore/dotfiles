bf6366f7e8a5fbb0b806b088d71027825a3bd51c
Gregory Wood <greg@wood.name>
2014-11-16 10:00:53 -0500

    Silence tldr updating

--- script/npm.sh
+++ script/npm.sh
@@ -16,7 +16,7 @@ source "$DOTFILES_ROOT/script/lib.sh"
 if ! which tldr > /dev/null; then
 	npm install -g tldr  >> "$DOTFILES_ROOT/npm.log" 2>&1
 fi
-tldr --update
+tldr --update > /dev/null 2>&1
 
 if ! which ember > /dev/null; then
 	npm install -g ember-cli >> "$DOTFILES_ROOT/npm.log" 2>&1
