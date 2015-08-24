eefecfeb2e3fdd1ddd039cb2bf3d007fe219f8c7
Gregory Wood <greg@wood.name>
2015-08-24 15:02:28 -0400

    Add pause to ensure closing the app doesn't overwrite subsequent settings

--- script/apps/iterm2.sh
+++ script/apps/iterm2.sh
@@ -4,5 +4,8 @@
 # Install the Solarized Dark theme for iTerm
 open "$DOTFILES_ROOT/init/Solarized Dark.itermcolors"
 
+# Allow user to close iTerm2 before writing any more settings
+pause "Press any key to continue after iTerm2 is closed..."
+
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
