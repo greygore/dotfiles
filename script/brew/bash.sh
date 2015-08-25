646bd209c14934a1e0b4ed42e7fa7f0d7e10fa63
Gregory Wood <greg@wood.name>
2015-08-25 00:51:10 -0400

    Update bash completion to version 2 which supports bash 4

--- script/brew/bash.sh
+++ script/brew/bash.sh
@@ -6,10 +6,13 @@ source "$DOTFILES_ROOT/script/brew/lib.sh"
 # Initialize sudo password save
 init_sudo; destroy_sudo
 
-# Modern bash & completion
+# Modern bash
 brew_formula 'bash'
 echo '/usr/local/bin/bash' | sudo tee -a /etc/shells > /dev/null
 asudo chsh -s /usr/local/bin/bash "$USER" > /dev/null 2>&1 \
 && success 'Updated shell to brew version of bash.' \
 || error 'Unable to update shell to brewed version of bash'
-brew_formula 'bash-completion'
+
+# Bash completion
+brew tap homebrew/versions
+brew_formula 'bash-completion2'
