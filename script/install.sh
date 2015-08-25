114188dc0dbcf0da0bcfc91f36c67665a512d261
Gregory Wood <greg@wood.name>
2015-08-25 16:27:20 -0400

    Fix syntax error

--- script/install.sh
+++ script/install.sh
@@ -74,7 +74,7 @@ if [ ! -f "$HOME/.ssh/id_rsa.pub" ]; then
 	fi
 	open https://www.github.com/settings/ssh
 	pause 'Enter your public key to your Github account, then press any key to continue.'
-elif confirm 'Would you like to enter your public key on Github?'
+elif confirm 'Would you like to enter your public key on Github?'; then
 	if is_osx; then
 		pbcopy < "$HOME/.ssh/id_rsa.pub"
 		info 'Your public key has been copied to your clipboard.'
