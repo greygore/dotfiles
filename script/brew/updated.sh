c1f9162611c49c411eb96d815bd3cda85a00155e
Gregory Wood <greg@wood.name>
2015-08-25 00:22:43 -0400

    Brew changed --default-names to --with-default-names

--- script/brew/updated.sh
+++ script/brew/updated.sh
@@ -5,7 +5,7 @@ source "$DOTFILES_ROOT/script/brew/lib.sh"
 
 # Updated OSX binaries
 brew_formula 'coreutils'
-brew_formula 'findutils' '--default-names' # find, locate, updatedb, xargs
-brew_formula 'gnu-sed' '--default-names' # sed
+brew_formula 'findutils' '--with-default-names' # find, locate, updatedb, xargs
+brew_formula 'gnu-sed' '--with-default-names' # sed
 brew_formula 'git' '--with-gettext --with-pcre'
 
