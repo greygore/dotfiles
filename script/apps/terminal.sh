8827af2791167e62e43f88abcb4549204fd8bfd2
Gregory Wood <greg@wood.name>
2015-08-24 22:56:00 -0400

    Remove terrible hack to try and get Terminal to use Solarized theme
    From https://github.com/mathiasbynens/dotfiles/pull/492/files

--- script/apps/terminal.sh
+++ script/apps/terminal.sh
@@ -5,14 +5,38 @@
 defaults write com.apple.terminal StringEncodings -array 4
 
 # Use a modified version of the Solarized Dark theme by default in Terminal.app
-TERM_PROFILE='Solarized Dark xterm-256color';
-CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
-if [ "$CURRENT_PROFILE" != "$TERM_PROFILE" ]; then
-	(crontab -l 2> /dev/null | grep -v "terminal_cron.sh"; echo "@reboot $DOTFILES_ROOT/script/apps/terminal_cron.sh") | crontab -
-	sleep 5
-	(crontab -l 2> /dev/null | grep -v "terminal_cron.sh"; echo "@reboot $DOTFILES_ROOT/script/apps/terminal_cron.sh") | crontab -
-	open "$DOTFILES_ROOT/init/$TERM_PROFILE.terminal";
-fi;
+osascript <<EOD
+tell application "Terminal"
+	local allOpenedWindows
+	local initialOpenedWindows
+	local windowID
+	set themeName to "Solarized Dark xterm-256color"
+	(* Store the IDs of all the open terminal windows. *)
+	set initialOpenedWindows to id of every window
+	(* Open the custom theme so that it gets added to the list
+	   of available terminal themes (note: this will open two
+	   additional terminal windows). *)
+	do shell script "open '$HOME/init/" & themeName & ".terminal'"
+	(* Wait a little bit to ensure that the custom theme is added. *)
+	delay 1
+	(* Set the custom theme as the default terminal theme. *)
+	set default settings to settings set themeName
+	(* Get the IDs of all the currently opened terminal windows. *)
+	set allOpenedWindows to id of every window
+	repeat with windowID in allOpenedWindows
+		(* Close the additional windows that were opened in order
+		   to add the custom theme to the list of terminal themes. *)
+		if initialOpenedWindows does not contain windowID then
+			close (every window whose id is windowID)
+		(* Change the theme for the initial opened terminal windows
+		   to remove the need to close them in order for the custom
+		   theme to be applied. *)
+		else
+			set current settings of tabs of (every window whose id is windowID) to settings set themeName
+		end if
+	end repeat
+end tell
+EOD
 
 # Mouse focus for Terminal.app and all X11 apps
 # i.e. hover over a window and start typing in it with[out] clicking first
