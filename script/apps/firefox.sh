13ae585e567c2c1ee9260eef3a25ee6c06253b17
Gregory Wood <greg@wood.name>
2015-08-26 13:18:25 -0400

    Use fixed filenames for firefox extension files
    Because some Firefox extensions were downloading with extra parameter information (for example `adblock_plus-2.6.10-an+sm+tb+fx.xpi?filehash=sha256:3487c1da08f48f770fa8ce059bdb3f09e938cff5b4829c76c2fe391876d329d9`), the extension wasn't being loaded into Firefox or deleted afterwards. `--content-disposition` wasn't helping either, so it was easier just to arbitrarily set a filename for each extension.

--- script/apps/firefox.sh
+++ script/apps/firefox.sh
@@ -7,13 +7,13 @@ open -a Firefox --args -setDefaultBrowser
 pause 'Setting Firefox to your default browser. Press any key to continue installing addons.'
 
 # Install addons
-wget http://cdn.agilebits.com/dist/1P/ext/1Password-4.2.5.xpi > /dev/null 2>&1 # 1Password
-wget https://update.adblockplus.org/latest/adblockplusfirefox.xpi > /dev/null 2>&1 # AdBlock Plus
-wget http://download.xmarks.com/download/binary/firefox > /dev/null 2>&1 # XMarks
-wget https://addons.mozilla.org/firefox/downloads/latest/7661/addon-7661-latest.xpi > /dev/null 2>&1 # Pocket
-wget http://download.livereload.com/2.0.8/LiveReload-2.0.8.xpi > /dev/null 2>&1 # LiveReload
-wget https://addons.mozilla.org/firefox/downloads/file/276230/ember_inspector-1.5.0-fx.xpi > /dev/null 2>&1 # Ember Inspector
-wget https://addons.mozilla.org/firefox/downloads/latest/10869/addon-10869-latest.xpi > /dev/null 2>&1 # JSON View
+wget -O 1password.xpi http://cdn.agilebits.com/dist/1P/ext/1Password-4.2.5.xpi > /dev/null 2>&1 # 1Password
+wget -O adblock-plus.xpi https://update.adblockplus.org/latest/adblockplusfirefox.xpi > /dev/null 2>&1 # AdBlock Plus
+wget -O xmarks.xpi http://download.xmarks.com/download/binary/firefox > /dev/null 2>&1 # XMarks
+wget -O pocket.xpi https://addons.mozilla.org/firefox/downloads/latest/7661/addon-7661-latest.xpi > /dev/null 2>&1 # Pocket
+wget -O livereload.xpi http://download.livereload.com/2.0.8/LiveReload-2.0.8.xpi > /dev/null 2>&1 # LiveReload
+wget -O ember-inspector.xpi https://addons.mozilla.org/firefox/downloads/file/276230/ember_inspector-1.5.0-fx.xpi > /dev/null 2>&1 # Ember Inspector
+wget -O json-view.xpi https://addons.mozilla.org/firefox/downloads/latest/10869/addon-10869-latest.xpi > /dev/null 2>&1 # JSON View
 
 # TODO - Load addons without opening Firefox
 open -a Firefox.app *.xpi > /dev/null 2>&1
