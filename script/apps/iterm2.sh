da4400a9091ef999580de0461f7c0a2ea6fc96e6
Gregory Wood <greg@wood.name>
2015-08-26 10:40:25 -0400

    Use W flag to open instead of manually pausing

--- script/apps/iterm2.sh
+++ script/apps/iterm2.sh
@@ -2,10 +2,7 @@
 [ -z "$DOTFILES_ROOT" ] && ( echo "ERROR: DOTFILES_ROOT needs to be set"; exit 1 )
 
 # Install the Solarized Dark theme for iTerm
-open "$DOTFILES_ROOT/init/Solarized Dark.itermcolors"
-
-# Allow user to close iTerm2 before writing any more settings
-pause "Press any key to continue after iTerm2 is closed..."
+open -W "$DOTFILES_ROOT/init/Solarized Dark.itermcolors"
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
