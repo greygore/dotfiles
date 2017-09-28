a1b3d3395454a6cfd608ec099dbe0a1425089aee
Gregory Wood <greg@wood.name>
2017-09-27 23:49:47 -0400

    Add reminder to close iTerm

--- script/apps/iterm2.sh
+++ script/apps/iterm2.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 [ -z "$DOTFILES_ROOT" ] && ( echo "ERROR: DOTFILES_ROOT needs to be set"; exit 1 )
+source "$DOTFILES_ROOT/script/lib.sh"
 
 # Install the Solarized Dark theme for iTerm
+info 'Opening iTerm to install the Solarized Dark theme. Close iTerm to continue.'
 open -W "$DOTFILES_ROOT/init/Solarized Dark.itermcolors"
 
 # Don’t display the annoying prompt when quitting iTerm
