94e7963255a7fdcd412089501563f16023ac5117
Gregory Wood <greg@wood.name>
2017-09-28 01:02:55 -0400

    Update Firefox plugin list
    * Switch AdBlock Plus to uBlock Origin
    * Remove XMarks (possibly in favor of Firefox Sync)
    * Pocket is now included by default in Firefox
    * LiveReload appears to be defunct

--- script/apps/firefox.sh
+++ script/apps/firefox.sh
@@ -9,10 +9,7 @@ open -W -a Firefox --args -setDefaultBrowser
 # Install addons
 info 'Downloading Firefox extensions'
 wget -O 1password.xpi http://cdn.agilebits.com/dist/1P/ext/1Password-4.2.5.xpi > /dev/null 2>&1 # 1Password
-wget -O adblock-plus.xpi https://update.adblockplus.org/latest/adblockplusfirefox.xpi > /dev/null 2>&1 # AdBlock Plus
-wget -O xmarks.xpi http://download.xmarks.com/download/binary/firefox > /dev/null 2>&1 # XMarks
-wget -O pocket.xpi https://addons.mozilla.org/firefox/downloads/latest/7661/addon-7661-latest.xpi > /dev/null 2>&1 # Pocket
-wget -O livereload.xpi http://download.livereload.com/2.0.8/LiveReload-2.0.8.xpi > /dev/null 2>&1 # LiveReload
+wget -O ublock-origin.xpi https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-607454-latest.xpi?src=dp-btn-primary > /dev/null 2>&1 # uBlock Origin
 wget -O ember-inspector.xpi https://addons.mozilla.org/firefox/downloads/file/276230/ember_inspector-1.5.0-fx.xpi > /dev/null 2>&1 # Ember Inspector
 wget -O json-view.xpi https://addons.mozilla.org/firefox/downloads/latest/10869/addon-10869-latest.xpi > /dev/null 2>&1 # JSON View
 
